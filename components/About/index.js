6bd7ba2c3de5da9fe7af433101df05d4df033720
Shubhangkar <shubhangkar438@gmail.com>
2025-12-03 23:12:48 +0530

    Fixed some Bug and reshape

--- components/About/index.js
+++ components/About/index.js
@@ -48,12 +48,12 @@ const About = () => {
                         />
                     </h1>
                     <p>
-                        I'm a very ambitious front-end developer looking for a role in
+                        I&apos;m a very ambitious front-end developer looking for a role in
                         established IT company with the opportunity to work with the latest
                         technologies on chalenging and diverse project.
                     </p>
                     <p>
-                        I'm quietly confident, naturally curious, and perpetually working on
+                        I&apos;m quietly confident, naturally curious, and perpetually working on
                         improving my chops one design problem at a time.
                     </p>
                     <p>
